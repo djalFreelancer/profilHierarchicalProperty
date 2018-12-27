bab365fcadce4e81046f6958488f3890f5c398aa
djallal <djallalrahmoune@gmail.com>
2018-12-28 00:33:11 +0100

    done

---
+++ src/main/java/gul/in/profilAndHierarchicalProperties/ProfilAndHierarchicalPropertiesApplication.java
@@ -0,0 +1,14 @@
+package gul.in.profilAndHierarchicalProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilAndHierarchicalPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilAndHierarchicalPropertiesApplication.class, args);
+	}
+
+}
+
